016f861bb2d89dc87270107e8c9f840f59f91a38
Joseph Khawly <joseph.khawly@gmail.com>
2026-04-10 17:32:18 -0600

    chore: add turbopack cache in next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: 'export',
   images: {
     unoptimized: true,
   },
-};
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+    turbopackFileSystemCacheForBuild: true,
+  },
+}
 
 export default nextConfig;
